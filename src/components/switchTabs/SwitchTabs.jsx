ee3eade0dfaae8a050259d45f24a2f5801169072
Pritam <pritammagdum24@gmail.com>
2024-03-11 23:43:32 +0530

    Added SwitchTabs Component for Switching Movies or TV Shows as Day or Week

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./SwitchTabs.scss";
+const SwitchTabs = ({ data, onTabChange }) => {
+  return <div className="switchingTabs">SwitchTabs</div>;
+};
+
+export default SwitchTabs;
