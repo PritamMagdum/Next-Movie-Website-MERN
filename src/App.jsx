ee4f6d42c849befa666cf21d8dfe0bf141b084a5
Pritam <pritammagdum24@gmail.com>
2024-03-11 11:34:15 +0530

    Added HomeSlice for Fetching and Storing Home Page Related Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useEffect } from "react";
 import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
   };
-
-  return <div className="App">Hello World</div>;
+  return <div className="App">{url?.total_pages}</div>;
 }
 
 export default App;
