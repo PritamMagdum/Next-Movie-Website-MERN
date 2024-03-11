48ebfbc7ca321595552bfe08a8ee766b128f9bda
Pritam <pritammagdum24@gmail.com>
2024-03-11 21:52:29 +0530

    Added Routes with the Help of React Router Dom for Navigate the Dynamically on the Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { getApiConfiguration } from "./store/homeSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -10,16 +18,36 @@ function App() {
   console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
     });
   };
-  return <div className="App">{url?.total_pages}</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </div>
+  );
 }
 
 export default App;
