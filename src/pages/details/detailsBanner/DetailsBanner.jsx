bc5f5d952ff7bd76cd389b57a63d7c6c413d035f
Pritam <pritammagdum24@gmail.com>
2024-03-12 17:22:50 +0530

    Added VideoPopup Component for Trailer Watch Purpose with the Help of React-Player Library

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,11 +12,14 @@ import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../PlayBtn.jsx";
+import VideoPopup from "../../../components/videoPopup/VideoPopup.jsx";
 
 const DetailsBanner = ({ video, crew }) => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
 
   const _geners = data?.genres?.map((g) => g.id);
   const director = crew?.filter((f) => f.job === "Director");
@@ -61,7 +64,13 @@ const DetailsBanner = ({ video, crew }) => {
                     <Genres data={_geners} />
                     <div className="row">
                       <CircleRating rating={data.vote_average.toFixed(1)} />
-                      <div className="playbtn">
+                      <div
+                        className="playbtn"
+                        onClick={() => {
+                          setShow(true);
+                          setVideoId(video.key);
+                        }}
+                      >
                         <PlayIcon />
                         <span className="text">Watch Trailer</span>
                       </div>
@@ -71,13 +80,13 @@ const DetailsBanner = ({ video, crew }) => {
                       <div className="description">{data.overview}</div>
                     </div>
                     <div className="info">
-                      {data.status && (
+                      {data?.status && (
                         <div className="infoItem">
                           <span className="text bold">Status : </span>
                           <span className="text">{data.status}</span>
                         </div>
                       )}
-                      {data.release_date && (
+                      {data?.release_date && (
                         <div className="infoItem">
                           <span className="text bold">Release Date : </span>
                           <span className="text">
@@ -95,14 +104,42 @@ const DetailsBanner = ({ video, crew }) => {
                       )}
                     </div>
 
-                    {director.length > 0 && (
+                    {director?.length > 0 && (
                       <div className="info">
                         <div className="text bold">Director : </div>
                         <span className="text">
-                          {director.map((d, index) => (
+                          {director?.map((d, index) => (
                             <span key={index}>
                               {d.name}
-                              {director.length - 1 !== index && ", "}
+                              {director?.length - 1 !== index && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+
+                    {writer?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Writer :</span>
+                        <span className="text">
+                          {writer?.map((d, index) => (
+                            <span key={index}>
+                              {d.name}
+                              {writer?.length - 1 !== index && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+
+                    {data?.created_by?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Creator :</span>
+                        <span className="text">
+                          {data?.created_by?.map((d, index) => (
+                            <span key={index}>
+                              {d.name}
+                              {data?.created_by?.length - 1 !== index && ", "}
                             </span>
                           ))}
                         </span>
@@ -110,6 +147,12 @@ const DetailsBanner = ({ video, crew }) => {
                     )}
                   </div>
                 </div>
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                />
               </ContentWrapper>
             </React.Fragment>
           )}
